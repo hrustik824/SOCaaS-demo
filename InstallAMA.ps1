a1b6a0a3560bcebc12f44b522f6eaab6f24a85d2
hrustik824 <37297717+hrustik824@users.noreply.github.com>
2023-06-05 23:01:40 +0300

    Update InstallAMA.ps1

--- InstallAMA.ps1
+++ InstallAMA.ps1
@@ -1,2 +1,3 @@
-powershell -command "Invoke-WebRequest -Uri https://aka.ms/amagent -OutFile setup.exe"
-powershell -command "Start-Process -FilePath setup.exe -ArgumentList '/Q:A /R:N /C:\""setup.exe /qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_SUBSCRIPTION_ID=$(Subscription.ID) OPINSIGHTS_WORKSPACE_RESOURCE_ID=$(WorkspaceResource.ID) OPINSIGHTS_WORKSPACE_ID=$(Workspace.ID) OPINSIGHTS_WORKSPACE_KEY=$(Workspace.KEY) /l*v C:\TEMP\am.log\"" ' -Wait -NoNewWindow"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri https://aka.ms/amagent -OutFile setup.exe
+Start-Process -FilePath setup.exe -ArgumentList "@/Q:A /R:N /C:`"setup.exe /qn /norestart ADD_OPINSIGHTS_WORKSPACE_AZURE_SUBSCRIPTION_ID=$(Subscription.ID) ADD_OPINSIGHTS_WORKSPACE_RESOURCE_ID=$(WorkspaceResource.ID) ADD_OPINSIGHTS_WORKSPACE_ID=$(Workspace.ID) ADD_OPINSIGHTS_WORKSPACE_KEY=$(Workspace.KEY) `/l*v C:\install-mma.log`"" -Wait
